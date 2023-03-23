5ad2bf04f34909bd37a2defadec039da7d43d2f0
Raúl Heredia Horcajo <raulherediahorcajo@gmail.com>
2023-03-23 19:06:28 +0100

    test: Add SocietyCriteria tests

--- demo/src/test/java/com/example/demo/unit/repository/criteria/SocietyCriteriaUnitTest.java
+++ demo/src/test/java/com/example/demo/unit/repository/criteria/SocietyCriteriaUnitTest.java
@@ -4,7 +4,10 @@ import com.example.demo.repository.criteria.SocietyCriteria;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
 
 class SocietyCriteriaUnitTest {
 
@@ -35,5 +38,24 @@ class SocietyCriteriaUnitTest {
     void testSetName() {
         societyCriteria.setName("Name changed");
         assertEquals("Name changed", societyCriteria.getName());
+    }
+
+    @Test
+    void testEqualsAndHashCode() {
+        SocietyCriteria duplicatedSocietyCriteria = new SocietyCriteria("XXXXXXXXXX", "Test Society");
+        assertThat(societyCriteria.equals(duplicatedSocietyCriteria)).isTrue();
+        assertEquals(societyCriteria.hashCode(), duplicatedSocietyCriteria.hashCode());
+
+        /*Society differentSocietyName = new Society(1, "XXXXXXXXXX", "Distinct Society");
+        assertThat(society.equals(differentSocietyName)).isFalse();*/
+
+        SocietyCriteria distinctSocietyCriteria = new SocietyCriteria("YYYYYYYYYY", "Distinct Society");
+        assertThat(societyCriteria.equals(distinctSocietyCriteria)).isFalse();
+        assertNotEquals(societyCriteria.hashCode(), distinctSocietyCriteria.hashCode());
+
+        assertThat(societyCriteria.equals(societyCriteria)).isTrue();
+        assertThat(societyCriteria.equals(null)).isFalse();
+        assertThat(societyCriteria.equals(new Object())).isFalse();
+        assertThat(societyCriteria.equals(mock(SocietyCriteria.class))).isFalse();
     }
 }
