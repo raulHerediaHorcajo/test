12acc735ad831ec51ba7fa4c35bf80a775ac5866
Raúl Heredia Horcajo <raulherediahorcajo@gmail.com>
2023-05-10 15:15:59 +0200

    feat: Add JWT Auth components

---
+++ demo/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.demo.security;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email" + email + "not found"));
+
+        List<? extends GrantedAuthority> grantedAuthorityList = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_"+role))
+                .toList();
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), grantedAuthorityList);
+    }
+}
---
+++ demo/src/main/java/com/example/demo/security/jwt/component/JwtCookieManager.java
@@ -0,0 +1,27 @@
+package com.example.demo.security.jwt.component;
+
+import com.example.demo.security.jwt.util.SecurityCipher;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtCookieManager {
+
+	public static final String ACCESS_TOKEN_COOKIE_NAME = "AuthToken";
+	public static final String REFRESH_TOKEN_COOKIE_NAME = "RefreshToken";
+
+	public HttpCookie createAccessTokenCookie(String token) {
+		String encryptedToken = SecurityCipher.encrypt(token);
+		return ResponseCookie.from(ACCESS_TOKEN_COOKIE_NAME, encryptedToken).maxAge(-1).httpOnly(true).path("/").build();
+	}
+
+	public HttpCookie createRefreshTokenCookie(String token) {
+		String encryptedToken = SecurityCipher.encrypt(token);
+		return ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, encryptedToken).maxAge(-1).httpOnly(true).path("/").build();
+	}
+
+	public HttpCookie deleteAccessTokenCookie() {
+		return ResponseCookie.from(ACCESS_TOKEN_COOKIE_NAME, "").maxAge(0).httpOnly(true).path("/").build();
+	}
+}
