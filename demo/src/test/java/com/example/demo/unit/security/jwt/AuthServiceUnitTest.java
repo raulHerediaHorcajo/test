df5fbda3ce2db7e2e04a0ef430fe5d3f0901f1e3
Raúl Heredia Horcajo <raulherediahorcajo@gmail.com>
2023-05-23 13:56:19 +0200

    test: Add AuthService tests

---
+++ demo/src/test/java/com/example/demo/unit/security/jwt/AuthServiceUnitTest.java
@@ -0,0 +1,210 @@
+package com.example.demo.unit.security.jwt;
+
+import com.example.demo.security.jwt.AuthService;
+import com.example.demo.security.jwt.component.JwtCookieManager;
+import com.example.demo.security.jwt.component.JwtTokenProvider;
+import com.example.demo.security.jwt.dto.AuthResponse;
+import com.example.demo.security.jwt.dto.LoginRequest;
+import com.example.demo.security.jwt.dto.Token;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceUnitTest {
+
+    private AuthService authService;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+    @Mock
+    private UserDetailsService userDetailsService;
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+    @Mock
+    private JwtCookieManager cookieUtil;
+
+    @BeforeEach
+    public void setUp() {
+        authService = new AuthService(
+            authenticationManager,
+            userDetailsService,
+            jwtTokenProvider,
+            cookieUtil);
+    }
+
+    @Test
+    void whenLoginWithInvalidAccessTokenAndInvalidRefreshToken_thenShouldGiveAuthResponse() {
+        LoginRequest loginRequest = new LoginRequest("username", "password");
+        String encryptedAccessToken = "encryptedAccessToken";
+        String encryptedRefreshToken = "encryptedRefreshToken";
+        when(authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(),
+                loginRequest.getPassword())
+        )).thenReturn(mock(Authentication.class));
+        UserDetails user = mock(UserDetails.class);
+        when(userDetailsService.loadUserByUsername(loginRequest.getUsername())).thenReturn(user);
+        when(jwtTokenProvider.validateToken(any())).thenReturn(false);
+        when(jwtTokenProvider.validateToken(any())).thenReturn(false);
+        Token newAccessToken = mock(Token.class);
+        Token newRefreshToken = mock(Token.class);
+        when(jwtTokenProvider.generateToken(user)).thenReturn(newAccessToken);
+        when(jwtTokenProvider.generateRefreshToken(user)).thenReturn(newRefreshToken);
+        HttpCookie httpCookieAccessToken = mock(HttpCookie.class);
+        when(httpCookieAccessToken.toString()).thenReturn("eyXXXXXXXXXXXXXX");
+        HttpCookie httpCookieRefreshToken = mock(HttpCookie.class);
+        when(httpCookieRefreshToken.toString()).thenReturn("eyYYYYYYYYYYYYYY");
+        when(cookieUtil.createAccessTokenCookie(newAccessToken.getTokenValue())).thenReturn(httpCookieAccessToken);
+        when(cookieUtil.createRefreshTokenCookie(newRefreshToken.getTokenValue())).thenReturn(httpCookieRefreshToken);
+
+        ResponseEntity<AuthResponse> result = authService.login(loginRequest, encryptedAccessToken, encryptedRefreshToken);
+
+        verify(authenticationManager).authenticate(
+            new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(),
+                loginRequest.getPassword())
+        );
+        verify(userDetailsService).loadUserByUsername(loginRequest.getUsername());
+        verify(jwtTokenProvider, times(2)).validateToken(any());
+        verify(jwtTokenProvider).generateToken(user);
+        verify(jwtTokenProvider).generateRefreshToken(user);
+        verify(cookieUtil).createAccessTokenCookie(newAccessToken.getTokenValue());
+        verify(cookieUtil).createRefreshTokenCookie(newRefreshToken.getTokenValue());
+        assertThat(result).isNotNull();
+        assertThat(result.getHeaders()).isNotNull();
+        assertThat(result.getHeaders().get(HttpHeaders.SET_COOKIE))
+            .isNotNull()
+            .containsAll(List.of("eyXXXXXXXXXXXXXX", "eyYYYYYYYYYYYYYY"));
+        assertThat(result.getBody()).isNotNull();
+        assertThat(result.getBody().getStatus()).isEqualTo(AuthResponse.Status.SUCCESS);
+        assertThat(result.getBody().getMessage()).isEqualTo("Auth successful. Tokens are created in cookie.");
+    }
+
+    /*@Test
+    void testFindAll() {
+        SocietyCriteria filters = mock(SocietyCriteria.class);
+        Specification<Society> specification = new SocietySpecification(filters);
+        Pageable pageable = PageRequest.of(0, 20);
+        List<Society> societies = List.of(
+            new Society("XXXXXXXXXX","Test Society 1"),
+            new Society("YYYYYYYYYY","Test Society 2"),
+            new Society("ZZZZZZZZZZ","Test Society 3")
+        );
+        Page<Society> page = new PageImpl<>(societies, pageable, 3);
+
+        when(societyRepository.findAll(specification, pageable)).thenReturn(page);
+
+        Page<Society> result = authService.findAll(filters, pageable);
+
+        verify(societyRepository).findAll(specification, pageable);
+        assertThat(result).isNotNull();
+        assertThat(result.getNumberOfElements()).isEqualTo(3);
+        assertThat(result.getContent()).containsAll(societies);
+        assertThat(result.getPageable()).isEqualTo(pageable);
+    }
+
+    @Test
+    void whenFindByIdSocietyDoesNotExist_thenShouldGiveOptionalEmpty() {
+        when(societyRepository.findById((long) 1)).thenReturn(Optional.empty());
+
+        Optional<Society> resultSociety = authService.findById(1);
+
+        verify(societyRepository).findById((long) 1);
+        assertThat(resultSociety)
+            .isNotPresent();
+    }
+
+    @Test
+    void testFindById() {
+        Society expectedSociety = new Society(1, "cifDni", "name");
+
+        when(societyRepository.findById((long) 1)).thenReturn(Optional.of(expectedSociety));
+
+        Optional<Society> resultSociety = authService.findById(1);
+
+        verify(societyRepository).findById((long) 1);
+        assertThat(resultSociety)
+            .isPresent()
+            .contains((expectedSociety));
+    }
+
+    @Test
+    void testAddSociety(){
+        Society society = new Society("cifDni", "name");
+        Society expectedSociety = new Society(1, "cifDni", "name");
+
+        when(societyRepository.save(society)).thenReturn(expectedSociety);
+
+        Society resultSociety = authService.addSociety(society);
+
+        verify(societyRepository).save(society);
+        assertThat(resultSociety).isEqualTo(expectedSociety);
+    }
+
+    @Test
+    void whenUpdateSocietyDoesNotExist_thenShouldGiveSocietyNotFoundException() {
+        Society newSociety = new Society("newCifDni", "newName");
+        when(societyRepository.findById((long) 1)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> authService.updateSociety(1, newSociety))
+            .isInstanceOf(SocietyNotFoundException.class)
+            .hasMessageContaining("Society 1 not found");
+
+        verify(societyRepository).findById((long) 1);
+        verify(societyRepository, never()).save(any(Society.class));
+    }
+
+    @Test
+    void testUpdateSociety() {
+        Society newSociety = new Society("newCifDni", "newName");
+        Society storedSociety = new Society(1, "cifDni", "name");
+        Society expectedSociety = new Society(1, "newCifDni", "newName");
+        when(societyRepository.findById((long) 1)).thenReturn(Optional.of(storedSociety));
+        when(societyRepository.save(newSociety)).thenReturn(expectedSociety);
+
+        Society resultSociety = authService.updateSociety(1, newSociety);
+
+        verify(societyRepository).findById((long) 1);
+        verify(societyRepository).save(newSociety);
+        assertThat(resultSociety).isEqualTo(expectedSociety);
+    }
+
+    @Test
+    void whenDeleteSocietyDoesNotExist_thenShouldGiveSocietyNotFoundException() {
+        when(societyRepository.findById((long) 1)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> authService.deleteSociety(1))
+            .isInstanceOf(SocietyNotFoundException.class)
+            .hasMessageContaining("Society 1 not found");
+
+        verify(societyRepository).findById((long) 1);
+        verify(societyRepository, never()).delete(any(Society.class));
+    }
+
+    @Test
+    void testDeleteSociety() {
+        Society society = new Society(1, "cifDni", "name");
+        when(societyRepository.findById((long) 1)).thenReturn(Optional.of(society));
+
+        authService.deleteSociety(1);
+
+        verify(societyRepository).findById((long) 1);
+        verify(societyRepository).delete(society);
+    }*/
+}
