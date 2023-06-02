4114210896d7b3bcb22996274032d515d0a5ff53
Raúl Heredia Horcajo <raulherediahorcajo@gmail.com>
2023-06-02 12:38:36 +0200

    test: Add UserRestController tests

--- demo/src/test/java/com/example/demo/config/TestConfig.java
+++ demo/src/test/java/com/example/demo/config/TestConfig.java
@@ -1,2 +1,56 @@
-package com.example.demo.config;public class TestConfig {
+package com.example.demo.config;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@TestConfiguration
+public class TestConfig {
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+    private DatabasePopulator databasePopulator() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("InitializationTestData.sql"));
+        return populator;
+    }
+
+
+    /*@Value("${spring.datasource.url}")
+    private static String url;
+    @Value("${spring.datasource.username}")
+    private static String username;
+    @Value("${spring.datasource.password}")
+    private static String password;
+
+    @BeforeAll
+    public static void setupDatabase() {
+        DataSource dataSource = DataSourceBuilder.create()
+            .url(url)
+            .username(username)
+            .password(password)
+            .build();
+
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("InitializationTestData.sql"));
+        populator.execute(dataSource);
+    }
+
+    // ...*/
 }
